13c7cdc445ffce36339adbe8c52fe6a0b82fa412
SolveigRebnord <91594496+SolveigRebnord@users.noreply.github.com>
2023-04-11 00:17:17 +0200

    Footer finished

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,47 @@
 
 import tw from "tailwind-styled-components"
+import { NavLink } from "react-router-dom";
+import {UnderlineLink} from '../styles/Buttons'
+
 
 const StyleFooter = tw.footer`
     bg-mainBeige
     w-full
-    h-52
+    h-64
     mt-8
+    p-8
+    pt-14
+    md:pt-20
+    flex
+    flex-col
+    justify-between
+    items-center
 `
 
 const Footer = () => {
     return ( 
         <>
         <StyleFooter>
-         <h3>Footer</h3>
+            <div className="">
+                <nav>
+                    <ul className="flex flex-col items-center gap-6 md:flex-row md:gap-20">
+                        <li>
+                            <NavLink to={'/products'} >
+                                <UnderlineLink>All products</UnderlineLink>
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to={'/contact'} >
+                                <UnderlineLink>Contact</UnderlineLink>
+                            </NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+          
+         <div className="">
+            <p className=" ">© 2021 - 2023 AssortedStuff, Inc. All rights reserved</p>
+         </div>
         </StyleFooter>
         </>
      );
